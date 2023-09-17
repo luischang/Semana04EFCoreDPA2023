da2decd624cfc254c3322c8a3d1954ac155f33d5
laboratorioesan <laboratorioesan@WS2302537>
2023-09-16 20:36:09 -0500

    Agregar archivos de proyecto.

---
+++ Semana04EFCore.API/Data/People.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Semana04EFCore.API.Data;
+
+public partial class People
+{
+    public int Id { get; set; }
+
+    public string? Fullname { get; set; }
+
+    public string? Email { get; set; }
+
+    public int? Dni { get; set; }
+}
---
+++ Semana04EFCore.API/Data/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Semana04EFCore.API.Data;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal? UnitPrice { get; set; }
+}
